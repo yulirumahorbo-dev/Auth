2d2cfd20180c0b28daa7b154ab10b0165b636e81
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-03 19:54:50 +0700

    feat: Creating new users

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyANYcMKWSXi1dAfb5ZGRXwp83zTW1UfekE";
 
-async function createUser() {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
