4a72447371201a36b5b993bab091c021e7cf92a1
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-03-04 10:24:51 +0700

    feat: Accessing protected resources using token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://auth-8b5c1-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
